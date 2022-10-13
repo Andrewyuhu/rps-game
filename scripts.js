af4ad0e1e9a4f85b160301d32943d3920f7e044d
Andrew Hu <andrewyuhu02@gmail.com>
2022-10-12 18:37:33 -0600

    Added a minimal GUI and some basic animations

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 function playRound (userChoice) {
     let computerChoice = generateComputerChoice();
+    displayScore();
     checkWinner(userChoice, computerChoice);
 }
 
@@ -8,37 +9,40 @@ function generateComputerChoice() {
 }
 
 function displayScore () {
-    playerScoreDisplay.textContent = `Player score ${playerScore}`;
-    computerScoreDisplay.textContent = `Computer score ${computerScore}`;
+    playerScoreDisplay.textContent = `Player score : ${playerScore}`;
+    computerScoreDisplay.textContent = `Computer score : ${computerScore}`;
 }
 
 function resetScore() {
     playerScore = 0;
     computerScore = 0;
     gameRound = 1;
-    playerScoreDisplay.textContent = `Player score ${playerScore}`;
-    computerScoreDisplay.textContent = `Computer score ${computerScore}`;
+    displayScore();
   
 }
 
 function checkWinner (userChoice, computerChoice) {
     let gameOutcome = winningCombinations[computerChoice][userChoice];
     resultsDisplay.textContent = "";
+    
     round.textContent = `Round ${gameRound}`
     gameRound++;
     switch(true){
         case gameOutcome == "w":
             playerScore++;
             displayScore();
-            resultsDisplay.textContent = "Player wins this round."
+            resultsDisplay.innerHTML = winnerMessage;
+            handConversions[userChoice].setAttribute("style","animation:win 0.75s;");
             break;
         case gameOutcome == "l":
             computerScore++;
             displayScore();
-            resultsDisplay.textContent = "Computer wins this round."
+            handConversions[userChoice].setAttribute("style","animation:shakeLose 0.75s;box-shadow: 0 8px 10px 0  #cc9999, 0px -8px 10px 0px  #cc9999;")
+            resultsDisplay.innerHTML = loserMessage;
             break;
         case gameOutcome == "d":
-            resultsDisplay.textContent = ("This round was a draw.")
+            handConversions[userChoice].setAttribute("style","animation:draw 0.75s;")
+            resultsDisplay.innerHTML = drawMessage;
     }
 
     if (playerScore == 5){
@@ -50,8 +54,10 @@ function checkWinner (userChoice, computerChoice) {
     }
 }
 
-const winningCombinations = [['d','w','l'],['l','d','w'],['w','l','d']];
 
+
+
+const winningCombinations = [['d','w','l'],['l','d','w'],['w','l','d']];
 let computerScore = 0;
 let playerScore = 0;
 let gameRound = 1;
@@ -61,9 +67,15 @@ const scissors = document.querySelector(".scissors");
 const resultsDisplay = document.querySelector(".results");
 const playerScoreDisplay = document.querySelector(".userScore");
 const computerScoreDisplay = document.querySelector(".computerScore");
+const handConversions = [rock,paper,scissors];
 const round = document.querySelector('.round');
-
+const winnerMessage = "Player <span style='color:rgb(118, 231, 118);'>wins</span> this round."
+const loserMessage = "Player <span style='color:red;'>loses</span> this round."
+const drawMessage = "Rounds ends in a <span style='color:rgb(223, 223, 73);'>draw</span>."
 rock.addEventListener("click",() => (playRound(0)) );
 paper.addEventListener("click", () => (playRound(1)) );
 scissors.addEventListener("click", () => (playRound(2)) );
 
+rock.addEventListener("animationend", () => {rock.removeAttribute("style");});
+paper.addEventListener("animationend", () => {paper.removeAttribute("style");});
+scissors.addEventListener("animationend", () => {scissors.removeAttribute("style");});
